b0cacb63968f4a9c119edeb27cb0abc9e4538ed1
fluffygriffin01 <urenajulian084@gmail.com>
2025-10-29 11:54:44 -0700

    SQLConnection factory created. Service added as singleton to API

--- MyGuitarShop.Api/Program.cs
+++ MyGuitarShop.Api/Program.cs
@@ -1,36 +1,67 @@
 
+
+using Microsoft.Data.SqlClient;
+using MyGuitarShop.Data.Ado.Factories;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
 namespace MyGuitarShop.Api
 {
-    public class Program
+    public static class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
+            try
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+                var builder = WebApplication.CreateBuilder(args);
+                AddServices(builder);
 
+                // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                if (builder.Environment.IsDevelopment())
+                {
+                    builder.Services.AddEndpointsApiExplorer();
+                    builder.Services.AddSwaggerGen();
+                }
+
+                var app = builder.Build();
+
+                // Configure the HTTP request pipeline.
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                ConfigureApplication(app);
+
+                await app.RunAsync();
+            }
+            catch (Exception ex)
+            {
+                if (Debugger.IsAttached)
+                    Debugger.Break();
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void ConfigureApplication(WebApplication app)
+        {
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
 
-
             app.MapControllers();
+        }
 
-            app.Run();
+        private static void AddServices(WebApplicationBuilder builder)
+        {
+            var connectionString = builder.Configuration.GetConnectionString(name: "MyGuitarShopDatabase")
+                ?? throw new InvalidOperationException("MyGuitarShop connection string not found.");
+
+            builder.Services.AddSingleton(new SqlConnectionFactory(connectionString)));
+
+            builder.Services.AddControllers();
         }
     }
 }
