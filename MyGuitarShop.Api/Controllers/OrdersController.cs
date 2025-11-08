cd44da81591e92c855af04f27703d888ec9fcf30
fluffygriffin01 <urenajulian084@gmail.com>
2025-11-08 10:42:07 -0800

    Created OrdersController.

---
+++ MyGuitarShop.Api/Controllers/OrdersController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyGuitarShop.Common.Dtos;
+using MyGuitarShop.Common.Interfaces;
+
+namespace MyGuitarShop.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController(
+        ILogger<OrdersController> logger,
+        IRepository<OrderDto> repo)
+        : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            try
+            {
+                var orders = await repo.GetAllAsync();
+                return Ok(orders);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error retrieving orders");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving orders");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            try
+            {
+                var order = await repo.FindByIdAsync(id);
+                if (order == null)
+                    return NotFound();
+
+                return Ok(order);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error retrieving order by ID");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error retrieving order by ID");
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateOrderAsync(OrderDto newOrder)
+        {
+            try
+            {
+                var numOrdersCreated = await repo.InsertAsync(newOrder);
+                return Ok($"{numOrdersCreated} new orders created");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error creating order");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error creating order");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateOrderAsync(int id, OrderDto updatedOrder)
+        {
+            try
+            {
+                if (await repo.FindByIdAsync(id) == null)
+                    return NotFound($"Order with id {id} not found");
+
+                var numOrdersUpdated = await repo.UpdateAsync(id, updatedOrder);
+                return Ok($"{numOrdersUpdated} orders updated");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error updating order");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error updating order");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteOrderAsync(int id)
+        {
+            try
+            {
+                if (await repo.FindByIdAsync(id) == null)
+                    return NotFound($"Order with id {id} not found");
+
+                var numOrdersDeleted = await repo.DeleteAsync(id);
+                return Ok($"{numOrdersDeleted} orders deleted");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Error deleting order");
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error deleting order");
+            }
+        }
+    }
+}
