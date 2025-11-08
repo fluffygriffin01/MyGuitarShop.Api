a42c1d43cf91d586738bcbda6470b1f4c53add3d
fluffygriffin01 <urenajulian084@gmail.com>
2025-11-08 08:01:49 -0800

    Created CategoryRepository.

---
+++ MyGuitarShop.Data.Ado/Repository/CategoryRepository.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Logging;
+using MyGuitarShop.Common.Dtos;
+using MyGuitarShop.Common.DTOs;
+using MyGuitarShop.Common.Interfaces;
+using MyGuitarShop.Data.Ado.Factories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGuitarShop.Data.Ado.Repository
+{
+    public class CategoryRepository(
+        ILogger<CategoryRepository> logger,
+        SqlConnectionFactory sqlConnectionFactory)
+        : IRepository<CategoryDto>
+    {
+        public async Task<IEnumerable<CategoryDto>> GetAllAsync()
+        {
+            var categories = new List<CategoryDto>();
+
+            try
+            {
+                await using var connection = await sqlConnectionFactory.OpenSqlConnectionAsync();
+                await using var command = new SqlCommand(cmdText: "SELECT * FROM Categories;", connection);
+                var reader = await command.ExecuteReaderAsync();
+
+                while (await reader.ReadAsync())
+                {
+                    var category = new CategoryDto
+                    {
+                        CategoryID = reader.GetInt32(reader.GetOrdinal("CategoryID")),
+                        CategoryName = reader.GetString(reader.GetOrdinal("CategoryName"))
+                    };
+                    categories.Add(category);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message, "Error retrieving category list");
+            }
+
+            return categories;
+        }
+
+        public async Task<int> InsertAsync(CategoryDto dto)
+        {
+            const string query = @"
+                INSERT INTO Categories (CategoryName)
+                VALUES (@CategoryName);";
+
+            try
+            {
+                await using var connection = await sqlConnectionFactory.OpenSqlConnectionAsync();
+                await using var command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@CategoryName", dto.CategoryName);
+
+                return await command.ExecuteNonQueryAsync();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message, "Error inserting new category");
+                return 0;
+            }
+        }
+
+        public async Task<CategoryDto?> FindByIdAsync(int id)
+        {
+            CategoryDto? category = null;
+
+            try
+            {
+                await using var connection = await sqlConnectionFactory.OpenSqlConnectionAsync();
+                await using var command = new SqlCommand(cmdText: "SELECT * FROM Categories WHERE CategoryID = @CategoryID;", connection);
+                command.Parameters.AddWithValue("@CategoryID", id);
+                var reader = await command.ExecuteReaderAsync();
+
+                if (await reader.ReadAsync())
+                {
+                    category = new CategoryDto
+                    {
+                        CategoryID = reader.GetInt32(reader.GetOrdinal("CategoryID")),
+                        CategoryName = reader.GetString(reader.GetOrdinal("CategoryName"))
+                    };
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message, $"Error retrieving category {id} by ID");
+            }
+            return category;
+        }
+
+        public async Task<int> UpdateAsync(int id, CategoryDto dto)
+        {
+            const string query = @"
+                UPDATE Categories
+                SET CategoryName = @CategoryName
+                WHERE CategoryID = @CategoryID; ";
+
+            try
+            {
+                await using var connection = await sqlConnectionFactory.OpenSqlConnectionAsync();
+                await using var command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@CategoryName", dto.CategoryName);
+                return await command.ExecuteNonQueryAsync();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message, $"Error updating category {id}");
+                throw;
+            }
+        }
+
+        public async Task<int> DeleteAsync(int id)
+        {
+            const string query = @"DELETE FROM Categories WHERE CategoryID = @CategoryID;";
+            try
+            {
+                await using var connection = await sqlConnectionFactory.OpenSqlConnectionAsync();
+                await using var command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@CategoryID", id);
+                return await command.ExecuteNonQueryAsync();
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message, $"Error deleting category {id}");
+                return 0;
+            }
+        }
+    }
+}
