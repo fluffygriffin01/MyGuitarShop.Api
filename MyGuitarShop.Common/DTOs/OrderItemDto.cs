2724af536ad4d6673e64118b03214e4b3d3e0d5d
fluffygriffin01 <urenajulian084@gmail.com>
2025-11-08 07:23:26 -0800

    Created OrderItemDto.

---
+++ MyGuitarShop.Common/DTOs/OrderItemDto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGuitarShop.Common.DTOs
+{
+    public class OrderItemDto
+    {
+        public required int ItemID { get; set; }
+        public int? OrderID { get; set; } = null;
+        public int? ProductID { get; set; } = null;
+        public required decimal ItemPrice { get; set; }
+        public required decimal DiscountAmount { get; set; }
+        public required int Quantity { get; set; }
+    }
+}
