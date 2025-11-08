77db511a7b61f08f711a394bb54b814b1703a4ec
fluffygriffin01 <urenajulian084@gmail.com>
2025-11-08 07:21:07 -0800

    Created CustomerDto.

---
+++ MyGuitarShop.Common/DTOs/CustomerDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGuitarShop.Common.DTOs
+{
+    public class CustomerDto
+    {
+        public required int CustomerID { get; set; }
+        [MaxLength(255)]
+        public required string EmailAddress { get; set; }
+        [MaxLength(60)]
+        public required string Password { get; set; }
+        [MaxLength(60)]
+        public required string FirstName { get; set; }
+        [MaxLength(60)]
+        public required string LastName { get; set; }
+        public int? ShippingAddressID { get; set; } = null;
+        public int? BillingAddressID { get; set; } = null;
+    }
+}
