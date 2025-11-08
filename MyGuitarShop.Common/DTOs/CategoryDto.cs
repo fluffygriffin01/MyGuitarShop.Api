ab72e95f625cfad04770b58149fed3a108b770a1
fluffygriffin01 <urenajulian084@gmail.com>
2025-11-08 07:18:47 -0800

    Created CategoryDto.

---
+++ MyGuitarShop.Common/DTOs/CategoryDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyGuitarShop.Common.DTOs
+{
+    public class CategoryDto
+    {
+        public required int CategoryID { get; set; }
+        [MaxLength(255)]
+        public required string CategoryName { get; set; }
+    }
+}
