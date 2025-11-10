3d4844e579aab105e94ca4fedffbfd6c49c4086a
fluffygriffin01 <urenajulian084@gmail.com>
2025-11-10 14:22:37 -0800

    Began changning OrderDto to use a transaction for mult-table insertion.

--- MyGuitarShop.Common/DTOs/OrderDto.cs
+++ MyGuitarShop.Common/DTOs/OrderDto.cs
@@ -5,18 +5,9 @@ namespace MyGuitarShop.Common.Dtos
     public class OrderDto
     {
         public int? OrderID { get; set; } = null;
-        public int? CustomerID { get; set; } = null;
-        public required DateTime OrderDate { get; set; }
-        public required decimal ShipAmount { get; set; }
-        public required decimal TaxAmount { get; set; }
-        public DateTime? ShipDate { get; set; }
-        public required int ShipAddressID { get; set; }
-        [MaxLength(50)]
-        public required string CardType { get; set; }
-        [MaxLength(16)]
-        public required string CardNumber { get; set; }
-        [MaxLength(7)]
-        public required string CardExpires { get; set; }
-        public required int BillingAddressID { get; set; }
+        public CustomerDto? Customer { get; set; } = null;
+        public required AddressDto ShippingAddress { get; set; }
+        public required AddressDto BillingAddress { get; set; }
+        public required List<OrderItemDto> Items { get; set; }
     }
 }
--- MyGuitarShop.Data.Ado/Repository/OrderRepository.cs
+++ MyGuitarShop.Data.Ado/Repository/OrderRepository.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Logging;
 using MyGuitarShop.Common.Dtos;
 using MyGuitarShop.Common.Interfaces;
 using MyGuitarShop.Data.Ado.Factories;
+using Newtonsoft.Json;
 
 namespace MyGuitarShop.Data.Ado.Repository
 {
@@ -51,23 +52,95 @@ namespace MyGuitarShop.Data.Ado.Repository
         public async Task<int> InsertAsync(OrderDto dto)
         {
             const string query = @"
-                INSERT INTO Orders (CustomerID, OrderDate, ShipAmount, TaxAmount, ShipDate, ShipAddressID, CardType, CardNumber, CardExpires, BillingAddressID)
-                VALUES (@CustomerID, @OrderDate, @ShipAmount, @TaxAmount, @ShipDate, @ShipAddressID, @CardType, @CardNumber, @CardExpires, @BillingAddressID);";
+                BEGIN TRY
+                    BEGIN TRANSACTION;
+
+                    DECLARE @BillingAddressID INT;
+                    DECLARE @ShippingAddressID INT;
+                    DECLARE @OrderID INT;
+
+                    -- Insert Billing Address
+                    INSERT INTO Addresses (CustomerID, Line1, Line2, City, State, ZipCode, Phone, Disabled)
+                    VALUES (@B_CustomerID, @B_Line1, @B_Line2, @B_City, @B_State, @B_ZipCode, @B_Phone, @B_Disabled);
+
+                    SET @BillingAddressID = SCOPE_IDENTITY();
+
+                    -- Insert Shipping Address
+                    INSERT INTO Addresses (CustomerID, Line1, Line2, City, State, ZipCode, Phone, Disabled)
+                    VALUES (@S_CustomerID, @S_Line1, @S_Line2, @S_City, @S_State, @S_ZipCode, @S_Phone, @S_Disabled);
+
+                    SET @ShippingAddressID = SCOPE_IDENTITY();
+
+                    -- Insert Order
+                    INSERT INTO Orders (CustomerID, OrderDate, ShipAmount, TaxAmount, ShipDate, ShipAddressID, CardType, CardNumber, CardExpires, BillingAddressID)
+                    VALUES (@CustomerID, @OrderDate, @ShipAmount, @TaxAmount, @ShipDate, @ShippingAddressID, @CardType, @CardNumber, @CardExpires, @BillingAddressID);
+
+                    SET @OrderID = SCOPE_IDENTITY();
+
+                    -- Insert Order Items
+                        INSERT INTO OrderItems (OrderID, ProductID, ItemPrice, DiscountAmount, Quantity)
+                        SELECT 
+                            @OrderID,
+                            ProductID,
+                            ItemPrice,
+                            DiscountAmount,
+                            Quantity
+                        FROM OPENJSON(@OrderItems)
+                        WITH (
+                            ProductID INT,
+                            ItemPrice DECIMAL(10,2),
+                            DiscountAmount DECIMAL(10,2),
+                            Quantity INT,
+                        );
+
+                    -- Commit if all succeeded
+                    COMMIT TRANSACTION;
+                END TRY
+                BEGIN CATCH
+                    ROLLBACK TRANSACTION;
+
+                    DECLARE @ErrorMessage NVARCHAR(4000) = ERROR_MESSAGE('Error creating order');
+                    DECLARE @ErrorSeverity INT = ERROR_SEVERITY();
+                    DECLARE @ErrorState INT = ERROR_STATE();
+                    RAISERROR (@ErrorMessage, @ErrorSeverity, @ErrorState);
+                END CATCH;";
 
             try
             {
                 await using var connection = await sqlConnectionFactory.OpenSqlConnectionAsync();
                 await using var command = new SqlCommand(query, connection);
-                command.Parameters.AddWithValue("@CustomerID", dto.CustomerID ?? (object)DBNull.Value);
+                command.Parameters.AddWithValue("@CustomerID", dto.Customer ?? (object)DBNull.Value);
+
+                // For billing address
+                command.Parameters.AddWithValue("@B_Line1", dto.BillingAddress.Line1);
+                command.Parameters.AddWithValue("@B_Line2", dto.BillingAddress.Line2);
+                command.Parameters.AddWithValue("@B_City", dto.BillingAddress.City);
+                command.Parameters.AddWithValue("@B_State", dto.BillingAddress.State);
+                command.Parameters.AddWithValue("@B_ZipCode", dto.BillingAddress.ZipCode);
+                command.Parameters.AddWithValue("@B_Phone", dto.BillingAddress.Phone);
+                command.Parameters.AddWithValue("@B_Disabled", dto.BillingAddress.Disabled);
+
+                // For shipping address
+                command.Parameters.AddWithValue("@S_Line1", dto.ShippingAddress.Line1);
+                command.Parameters.AddWithValue("@S_Line2", dto.ShippingAddress.Line2);
+                command.Parameters.AddWithValue("@S_City", dto.ShippingAddress.City);
+                command.Parameters.AddWithValue("@S_State", dto.ShippingAddress.State);
+                command.Parameters.AddWithValue("@S_ZipCode", dto.ShippingAddress.ZipCode);
+                command.Parameters.AddWithValue("@S_Phone", dto.ShippingAddress.Phone);
+                command.Parameters.AddWithValue("@S_Disabled", dto.ShippingAddress.Disabled);
+
+                // For order
                 command.Parameters.AddWithValue("@OrderDate", DateTime.UtcNow);
-                command.Parameters.AddWithValue("@ShipAmount", dto.ShipAmount);
-                command.Parameters.AddWithValue("@TaxAmount", dto.TaxAmount);
-                command.Parameters.AddWithValue("@ShipDate", dto.ShipDate ?? (object)DBNull.Value);
-                command.Parameters.AddWithValue("@ShipAddressID", dto.ShipAddressID);
-                command.Parameters.AddWithValue("@CardType", dto.CardType);
-                command.Parameters.AddWithValue("@CardNumber", dto.CardNumber);
-                command.Parameters.AddWithValue("@CardExpires", dto.CardExpires);
-                command.Parameters.AddWithValue("@BillingAddressID", dto.BillingAddressID);
+                command.Parameters.AddWithValue("@ShipAmount", 10);
+                command.Parameters.AddWithValue("@TaxAmount", 20);
+                command.Parameters.AddWithValue("@ShipDate", DBNull.Value);
+                command.Parameters.AddWithValue("@CardType", "Master");
+                command.Parameters.AddWithValue("@CardNumber", "1001");
+                command.Parameters.AddWithValue("@CardExpires", "04/2025");
+
+                // For order items
+                var jsonItems = JsonConvert.SerializeObject(dto.Items);
+                command.Parameters.AddWithValue("@OrderItems", jsonItems);
 
                 return await command.ExecuteNonQueryAsync();
             }
